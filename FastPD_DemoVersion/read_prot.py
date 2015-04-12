66d678d9fa8d203d9bb0d19e13d4715dd44cdbc9
adityamukund <adityamukund@gmail.com>
2015-04-12 02:15:25 -0400

    HOLY CRAP IT WORKED

--- FastPD_DemoVersion/read_prot.py
+++ FastPD_DemoVersion/read_prot.py
@@ -26,28 +26,6 @@ for i in xrange(len(costs)):
 pairs=array('l', [item for sublist in [[(i,j) for j in xrange(i+1, len(costs[0]))] for i in xrange(len(costs[0]))][:-1] for pair in sublist for tup in (pair, pair[::-1]) for item in tup])
 print("pairs: "+str(len(pairs)))
 
-<<<<<<< HEAD
-num=input("how long to wait: ")
-
-labels=label_file.readline().split('\t')
-costs=[map(float,line.split('\t')[:-1]) for line in cost_file]
-
-numpoints=bytes(len(costs[0]))
-numlabels=bytes(len(labels))
-lcosts=bytes(array('d', [costs[i][i] for i in xrange(len(costs[0]))]))
-pairs=bytes(array('d', [item for sublist in [[(i,j) for j in xrange(i+1, len(costs[0]))] for i in xrange(len(costs[0]))][:-1] for pair in sublist for item in pair]))
-numpairs=bytes(len(pairs))
-dist=bytes(array('d', [dist for lst in costs for dist in lst]))
-max_iters=bytes((30))
-wcosts=bytes(array('d', [1 for i in xrange(len(pairs))]))
-
-data = [numpoints, numlabels, lcosts, numpairs, pairs, dist, max_iters, wcosts]
-for item in data:
-	out_file.write(item)
-out_file.close()
-
-
-=======
 numpairs=len(pairs)/2
 print("numpairs: "+str(numpairs))
 
@@ -92,4 +70,3 @@ print(struct.unpack('l',r.read(8)))
 print(struct.unpack('l',r.read(8)))
 print(struct.unpack('l',r.read(8)))
 r.close()
->>>>>>> origin/adi
