3ef718b81bb1f23eaf2906ec03a72f78435dd4bc
adityamukund <adityamukund@gmail.com>
2015-04-13 14:03:29 -0400

    added multiple rotamer support

--- FastPD_DemoVersion/read_prot.py
+++ FastPD_DemoVersion/read_prot.py
@@ -7,40 +7,24 @@ label_file=open('/home/aditya/git/cs590/FastPD_DemoVersion/labels.txt', 'r')
 cost_file=open('/home/aditya/git/cs590/FastPD_DemoVersion/mutationEnergiesMin.txt', 'r')
 out_file=open('/home/aditya/git/cs590/FastPD_DemoVersion/matrices.bin', 'wb')
 
+counts=map(int, label_file.readline().split('\t'))
 labels=label_file.readline().split('\t')
 costs=[map(float,line.split('\t')[:-1]) for line in cost_file]
 
-###############################################################################################################################
-
-#numpoints=2
-#numlabels=5
-#labelings=[[1, 2, 9001, 9001, 9001], [9001, 9001, 1, 2, 3]]
-
-#lcosts=[0 for i in xrange(numlabels*numpoints)]
-#for i in xrange(len(labelings)):
-#        for k in xrange(len(labelings[i])):
-#                lcosts[k*numpoints+i]=labelings[i][k]
-#numpairs=1
-#pairs=[0, 1]
-
-#dist=[0 for i in xrange(25)]
-#for i in xrange(len(costs)):
-#        for j in xrange(len(costs[i])):
-#                dist[j*numlabels + i]=costs[i][j]
-
-#wcosts=[1]
-#max_iters=30
-
-################################################################################################################################
-numpoints=2
+numpoints=len(counts)
 numlabels=len(labels)
 
 internals=[costs[i][i] for i in xrange(len(costs))]
-label1 = internals[0:3]
-label1.extend([0 for x in xrange(3, len(internals))])
-label2 = [0 for x in xrange(3)]
-label2.extend(internals[3:])
-labelings=[label1, label2]
+labelings=[internals for i in xrange(numpoints)]
+for i in xrange(len(labelings)):
+	before=sum(counts[:i])
+	count=counts[i]
+	l = [9001 for x in xrange(before)]
+	l.extend(internals[before:before+count])
+	l.extend([9001 for x in xrange(before+count, numlabels)])
+	labelings[i]=l
+
+print(labelings)
 
 lcosts=[0 for i in xrange(numlabels*numpoints)]
 for i in xrange(len(labelings)):
@@ -60,36 +44,6 @@ for i in xrange(len(costs)):
 
 wcosts=[1 for i in xrange(numpairs)]
 max_iters=30
-
-
-################################################################################################################################
-
-
-
-#numpoints=2
-#numlabels=len(labels)
-
-#maxint=2147483647
-#internals=[int(round(costs[i][i])) for i in xrange(len(costs[0]))]
-#lcosts=[item for item in internals*len(costs[0])]
-
-#for i in xrange(len(costs)):
-#	costs[i][i]=0
-#pairs=array('l', [item for sublist in [[(i,j) for j in xrange(i+1, len(costs[0]))] for i in xrange(len(costs[0]))][:-1] for pair in sublist for item in pair])
-
-
-#numpairs=len(pairs)/2
-
-#dist = [0 for i in xrange(numlabels*numlabels)]
-#for i in xrange(len(costs)):
-#	for j in xrange(len(costs[i])):
-#		dist[i*numlabels + j] = int(round(costs[i][j]))
-#		if (j==0 and i!=0):
-#			dist[i*numlabels + j] = 2147483647
-
-#max_iters=30
-
-#wcosts=array('l', [1 for i in xrange(numpairs)])
 
 # The input binary file is assumed to contain all the necessary data for
 # defining the energy of a discrete MRF. 
@@ -113,3 +67,6 @@ out_file.write(struct.pack('='+'l'*len(pairs), *pairs))
 out_file.write(struct.pack('='+'l'*len(dist), *dist))
 out_file.write(struct.pack('='+'l'*len(wcosts), *wcosts))
 out_file.close()
+
+label_file.close()
+cost_file.close()
