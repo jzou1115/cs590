d042b310668a986720233bebd4d00a60f855dc7b
adityamukund <adityamukund@gmail.com>
2015-04-12 23:18:59 -0400

    BETTER THAN IT WAS THE LAST TIME BABY

--- FastPD_DemoVersion/read_prot.py
+++ FastPD_DemoVersion/read_prot.py
@@ -10,42 +10,86 @@ out_file=open('/home/aditya/git/cs590/FastPD_DemoVersion/matrices.bin', 'wb')
 labels=label_file.readline().split('\t')
 costs=[map(float,line.split('\t')[:-1]) for line in cost_file]
 
+###############################################################################################################################
+
+#numpoints=2
+#numlabels=5
+#labelings=[[1, 2, 9001, 9001, 9001], [9001, 9001, 1, 2, 3]]
+
+#lcosts=[0 for i in xrange(numlabels*numpoints)]
+#for i in xrange(len(labelings)):
+#        for k in xrange(len(labelings[i])):
+#                lcosts[k*numpoints+i]=labelings[i][k]
+#numpairs=1
+#pairs=[0, 1]
+
+#dist=[0 for i in xrange(25)]
+#for i in xrange(len(costs)):
+#        for j in xrange(len(costs[i])):
+#                dist[j*numlabels + i]=costs[i][j]
+
+#wcosts=[1]
+#max_iters=30
+
+################################################################################################################################
 numpoints=2
-print("numpoints: "+str(numpoints))
-
 numlabels=len(labels)
-print("numlabels: "+str(numlabels))
 
+internals=[costs[i][i] for i in xrange(len(costs))]
+label1 = internals[0:3]
+label1.extend([0 for x in xrange(3, len(internals))])
+label2 = [0 for x in xrange(3)]
+label2.extend(internals[3:])
+labelings=[label1, label2]
 
-maxint=2147483647
-lcosts=[0,0,0,maxint,maxint,maxint,maxint,maxint,maxint]
-internals=[int(round(costs[i][i])) for i in xrange(len(costs[0]))]
-internals[0]=2147483647
-#lcosts=[item for item in internals*len(costs[0])]
-print("lcosts: "+str(len(lcosts)))
+lcosts=[0 for i in xrange(numlabels*numpoints)]
+for i in xrange(len(labelings)):
+	for k in xrange(len(labelings[i])):
+		lcosts[k*numpoints+i]=labelings[i][k]
+
+pairs=[item for sublist in [[(i,j) for j in xrange(i+1, numpoints)] for i in xrange(len(costs[0]))][:-1] for pair in sublist for item in pair]
+numpairs=len(pairs)/2
 
 for i in xrange(len(costs)):
-	costs[i][i]=0
-pairs=array('l', [item for sublist in [[(i,j) for j in xrange(i+1, len(costs[0]))] for i in xrange(len(costs[0]))][:-1] for pair in sublist for item in pair])
-print("pairs: "+str(len(pairs)))
-print(pairs)
+	costs[i][i] = 0
 
-
-numpairs=len(pairs)/2
-print("numpairs: "+str(numpairs))
-
-dist = [0 for i in xrange(numlabels*numlabels)]
+dist=[0 for i in xrange(numlabels*numlabels)]
 for i in xrange(len(costs)):
 	for j in xrange(len(costs[i])):
-		dist[i*numlabels + j] = int(round(costs[i][j]))
-		if (j==0 and i!=0):
-			dist[i*numlabels + j] = 2147483647
-print("dist: "+str(len(dist)))
+		dist[j*numlabels + i] = costs[i][j]
 
+wcosts=[1 for i in xrange(numpairs)]
 max_iters=30
-print("max_iters: "+str(max_iters))
 
-wcosts=array('l', [1 for i in xrange(numpairs)])
+
+################################################################################################################################
+
+
+
+#numpoints=2
+#numlabels=len(labels)
+
+#maxint=2147483647
+#internals=[int(round(costs[i][i])) for i in xrange(len(costs[0]))]
+#lcosts=[item for item in internals*len(costs[0])]
+
+#for i in xrange(len(costs)):
+#	costs[i][i]=0
+#pairs=array('l', [item for sublist in [[(i,j) for j in xrange(i+1, len(costs[0]))] for i in xrange(len(costs[0]))][:-1] for pair in sublist for item in pair])
+
+
+#numpairs=len(pairs)/2
+
+#dist = [0 for i in xrange(numlabels*numlabels)]
+#for i in xrange(len(costs)):
+#	for j in xrange(len(costs[i])):
+#		dist[i*numlabels + j] = int(round(costs[i][j]))
+#		if (j==0 and i!=0):
+#			dist[i*numlabels + j] = 2147483647
+
+#max_iters=30
+
+#wcosts=array('l', [1 for i in xrange(numpairs)])
 
 # The input binary file is assumed to contain all the necessary data for
 # defining the energy of a discrete MRF. 
