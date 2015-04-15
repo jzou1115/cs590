4d193978aa1c1f516eac81cf81b77e0ae814c165
Jennifer Zou <jennifer.zou@duke.edu>
2015-04-14 21:39:52 -0400

    added pipeline (untested)

--- pipeline.sh
+++ pipeline.sh
@@ -1,6 +1,14 @@
-for dir in protein1 protein2; do 
+#Main directory: KStar.jar, Print.jar, read_prot.py, FastPD.exe, all of the protein files
+#Protein files: pdb file, KStar.cfg, System.cfg DEE_pert.cfg
+
+#Need to test!
+
+for dir in `ls * `; do 
 	cd $dir
-	python read_prot.py
-	wine FastPD.exe matrices.bin results.bin
+	java -jar KStar.jar -c KStar.cfg doDEE System.cfg DEE_pert.cfg
+	for dat in `ls *minM*.dat`; do 
+		java -jar ../Print.jar $dat
+		python ../read_prot.py $dat"_2d.txt"
+		wine ../FastPD.exe matrices.bin results.bin
 	cd ../
 done
