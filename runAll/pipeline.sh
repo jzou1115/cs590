cb10dd4686f67d67c64fc022ca29102466e250cb
Jennifer Zou <jennifer.zou@duke.edu>
2015-04-15 16:32:19 -0400

    changed names of config files

--- runAll/pipeline.sh
+++ runAll/pipeline.sh
@@ -1,17 +1,14 @@
 #Main directory: KStar.jar, Print.jar, read_prot.py, FastPD.exe, all of the protein files
 #Protein files: pdb file, KStar.cfg, System.cfg DEE_pert.cfg
 
-Syst=".pdbSystem.cfg"
-DEE=".pdbDEE.cfg"
-
 #for dir in 1AB2  1FSV  1NHZ  2EKO  2O2T; do 
 for dir in 1AB2; do 
 	cd $dir
-#	java -jar ../KStar.jar -c KStar.cfg fixStruct $dir".pdb" $dir"_fixed.pdb"
+#	java -jar ../KStar.jar -c KStar.cfg fixStruct $dir".pdb" fixed.pdb"
 	echo "checkPDB start"
-        python ../checkPDB.py $dir"_fixed.pdb" 3
+        python ../checkPDB.py fixed.pdb 3
 	echo "KStar.jar start"
-	java -jar ../KStar.jar -c KStar.cfg doDEE ${dir}${Syst} ${dir}${DEE}
+	java -jar ../KStar.jar -c KStar.cfg doDEE System.cfg DEE.cfg
 	echo "Print.jar start"
 	java -jar ../Print.jar min.dat
 	echo "read_prot.py start"
