4e0ee144420bab111afdb5ad6eb43aa988b1f4b6
Jennifer Zou <jennifer.zou@duke.edu>
2015-04-15 16:14:55 -0400

    add min energy matrix name

---
+++ runAll/checkPDB.py
@@ -0,0 +1,87 @@
+import argparse
+import random
+
+def getAtoms(pdb):
+    atoms=[]
+    for line in pdb:
+        tokens=line.split()
+        if(tokens[0].strip() == "ATOM"):
+            atoms.append(line)
+    return atoms
+
+
+#def checkAtoms(atoms):
+#    i = int(atoms[0].split()[1].strip())
+#    for a in atoms:
+#        print a
+#        if int(a.split()[1].strip()) != i:
+#            return False
+#        else:
+#            i+=1
+#    return True
+ 
+def checkAtoms(atoms):
+    return True
+
+def newPDB(pdb, f):
+    outfile = open(f, "w")
+    for l in pdb:
+        tokens=line.split()
+        if tokens[0].strip() != "TER":
+            outfile.write(l)
+    outfile.close() 
+
+def writeSysCfg(name, start, end, mut):
+    outfile = open(name+"System.cfg", "w")
+    outfile.write("pdbname "+name+"\n")
+    outfile.write("strand0 "+str(start)+" "+str(end)+"\n")
+    outfile.write("strandMutNums "+mut+"\n")
+    mutations = getMuts(start, end, int(mut))
+    outfile.write("strandMut0 "+mutations+"\n")
+    outfile.write("strandAA0  true\n")
+    outfile.write("strandRotTrans false\n")
+    outfile.write("onlySingleStrand 0\n")
+    outfile.write("numOfStrands 1")
+    outfile.close()
+
+def getMuts(start, end, mut):
+    mutations=[]
+    while len(mutations)< int(mut):
+        i = random.randrange(start, end)
+        if i not in mutations:
+            mutations.append(i)
+    ret=""
+    for m in mutations:
+        ret = ret+ str(m)+" "
+    return ret.strip()
+
+def writeDeeCfg(name, mut):
+    outfile = open(name+"DEE.cfg", "w")
+    outfile.write("runName "+name+"\n")
+    outfile.write("numMaxMut "+mut+"\n")
+    outfile.write("doMinimize false\n")
+    outfile.write("minEnergyMatrixName min.dat")
+    outfile.close()
+
+def main():
+    parser= argparse.ArgumentParser(description="Get file")
+    parser.add_argument("pdb")
+    parser.add_argument("mut")
+    args= parser.parse_args()
+
+    pdb= open(args.pdb, "r").readlines()
+    atoms= getAtoms(pdb)
+    if checkAtoms(atoms):
+	#write new pdf file w/0 TER
+	# newPDB(pdb, args.pdb+"terRemoved.pdb")
+
+        #write system.cfg
+        start = int(atoms[0].split()[1])
+        end = int(atoms[len(atoms)-1].split()[1])
+        writeSysCfg(args.pdb, start, end, args.mut)
+
+        #write dee.cfg
+        writeDeeCfg(args.pdb, args.mut)
+    
+
+main()
--- runAll/pipeline.sh
+++ runAll/pipeline.sh
@@ -1,17 +1,18 @@
 #Main directory: KStar.jar, Print.jar, read_prot.py, FastPD.exe, all of the protein files
 #Protein files: pdb file, KStar.cfg, System.cfg DEE_pert.cfg
 
-#Need to test!
+Syst=".pdbSystem.cfg"
+DEE=".pdbDEE.cfg"
 
-for dir in 1AB2  1FSV  1NHZ  2EKO  2O2T; do 
-#	echo $dir
+#for dir in 1AB2  1FSV  1NHZ  2EKO  2O2T; do 
+for dir in 1AB2; do 
 	cd $dir
-	java -jar KStar.jar -c KStar.cfg doDEE System.cfg DEE_pert.cfg
+#	java -jar ../KStar.jar -c KStar.cfg fixStruct $dir".pdb" $dir"_fixed.pdb"
+        python ../checkPDB.py $dir".pdb" 3
+	java -jar ../KStar.jar -c KStar.cfg doDEE ${dir}${Syst} ${dir}${DEE}
 	for dat in `ls *minM*.dat`; do 
-#	for dat in `ls *`; do
-#		echo $dat"_2d"
 		java -jar ../Print.jar $dat
-		python ../read_prot.py $dat"_2d.txt"
+		python ../read_prot.py "min.dat"
 		wine ../FastPD matrices.bin results.bin
 	cd ../
 	done
